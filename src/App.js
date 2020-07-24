1f61374abec96100eccc662af96362e5d4e34016
Chris <zari87@live.com>
2020-07-24 19:17:40 -0400

    Feat HandleSubmit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, { useState } from "react";
 
 import "./App.css";
 
+
+const handleSubmit = (e, notes, setNotes, taskName, setTaskName, dateTime, setdateTime) => {
+  e.preventDefault()
+  const id = (notes.length) ? notes[notes.length - 1].id + 1 : 1
+  setNotes([...notes, { id: id, message: taskName, deadline: dateTime }])
+  console.log("test", notes.length)
+  setdateTime('')
+  setTaskName('')
+}
 const TodoList = () => {
   const [taskName, setTaskName] = useState('')
   const [dateTime, setdateTime] = useState('')
+
+
   return (
     <div className="App">
       <h1>Chris Akua Todo</h1>
-      <form>
-        <input type="text" value={taskName} />
+      <form onSubmit={(e) => handleSubmit(e, taskName, setTaskName, dateTime, setdateTime)}>
+        <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)} />
         <input type="dateTime-local" onChange={(e) => setdateTime(e.target.value)} value={dateTime} />
+        <button>Submit</button>
       </form>
     </div>
   );
